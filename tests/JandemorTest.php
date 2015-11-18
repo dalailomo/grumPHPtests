3fce339ae6922d38c1a94fb33c4d909bae906c41
Peter <reisereise.85@gmail.com>
2015-11-18 15:47:39 +0000

    Trying to trigger errors in phpunit tests

---
+++ tests/JandemorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kapusta\tests;
+
+use Kapusta\Jandemor;
+
+class JandemorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Jandemor
+     */
+    private $sut;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->sut = new Jandemor();
+    }
+
+    /**
+     * @test
+     * @covers Jandemor::setKrander
+     */
+    public function testGetSetKrander()
+    {
+        $this->sut->setKrander('Hello world!');
+        $this->assertSame('Hello world!', $this->sut->getKrander());
+    }
+}
