7b9c34c78245d3f61f467b2d60e3473248963d3c
Peter <reisereise.85@gmail.com>
2015-11-19 09:15:13 +0000

    FOO-xx: triggering blacklist

--- Lib/Jandemor.php
+++ Lib/Jandemor.php
@@ -6,6 +6,13 @@ class Jandemor
 {
     protected $krander = 'Hello Default!';
 
+    /**
+     * Krander method to do krander things.
+     *
+     * @param null $krander
+     *
+     * @return null|string
+     */
     public function krander($krander = null)
     {
         if (isset($krander)) {
